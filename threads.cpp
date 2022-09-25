43ea3cc4d30714d94cad7891a1563a87d7d7f675
0AC0 <mihnea02@icloud.com>
2022-09-25 18:14:33 +0300

    Fixed dbg() program counter

--- threads.cpp
+++ threads.cpp
@@ -6,6 +6,8 @@ void Threads::start_all_cpus(Memory* m, uint64_t entry) {
 	instance().cpus.reserve(1);
 	instance().cpus[0].cpu = new CPU(m);
 	instance().cpus[0].thread = new std::thread(instance().cpus[0].cpu->run, instance().cpus[0].cpu, entry);
+	//TODO: this is hacky but temporary until we implement multi cpu support
+	dbg::regs = &(instance().cpus[0].cpu->regs);
 }
 
 void Threads::pause_all_cpus() {
