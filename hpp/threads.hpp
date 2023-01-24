8990e16dbe46946711b3f452c3fe793acec4c5fa
0AC0 <mihnea02@icloud.com>
2023-01-24 23:21:55 +0200

    add devices to threads and enable multicpu support

--- hpp/threads.hpp
+++ hpp/threads.hpp
@@ -3,7 +3,10 @@
 #include <vector>
 #include <thread>
 #include "cpu/cpu.hpp"
+#include "devices/device.hpp"
 
+class Memory;
+class CPU;
 
 class Threads {
 	private:
@@ -13,12 +16,19 @@ class Threads {
 			CPU* cpu;
 			std::thread* thread;
 		};
+		struct DeviceEntry {
+			Device* device;
+			std::thread* thread;
+		};
 		std::vector<CPUEntry> cpus;
+		std::vector<DeviceEntry> devices;
 
 		static void start_all_cpus(Memory* m, uint64_t entry);
 		static void join_all_cpus();
 		static void pause_all_cpus();
 		static void continue_all_cpus();
+
+		static void start_device(Device* d);
 
 		static Threads& instance() {
 			if (!t) t = new Threads();
--- threads.cpp
+++ threads.cpp
@@ -3,9 +3,11 @@
 Threads* Threads::t = 0;
 
 void Threads::start_all_cpus(Memory* m, uint64_t entry) {
-	instance().cpus.reserve(1);
-	instance().cpus[0].cpu = new CPU(m);
+	instance().cpus.reserve(2);
+	instance().cpus[0].cpu = new CPU(m, 0);
 	instance().cpus[0].thread = new std::thread(instance().cpus[0].cpu->run, instance().cpus[0].cpu, entry);
+	instance().cpus[1].cpu = new CPU(m, 1);
+	instance().cpus[1].thread = new std::thread(instance().cpus[1].cpu->run, instance().cpus[1].cpu, entry);
 	//TODO: this is hacky but temporary until we implement multi cpu support
 	dbg::regs = &(instance().cpus[0].cpu->regs);
 }
@@ -20,4 +22,12 @@ void Threads::continue_all_cpus() {
 
 void Threads::join_all_cpus() {
 	instance().cpus[0].thread->join();
+}
+
+void Threads::start_device(Device* d) {
+	DeviceEntry* de = new DeviceEntry;
+	de->device = d;
+	//de->thread = new std::thread(de->device->run);
+
+	instance().devices.push_back(*de);
 }
